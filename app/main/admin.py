242fc13297e010076a40c7c234fb3ec2969cd0b8
Odilxon <mmuhtor@gmail.com>
2022-05-29 19:42:57 +0500

    exclude geometry and password

--- app/main/admin.py
+++ app/main/admin.py
@@ -5,7 +5,7 @@ from flask import redirect, url_for
 from flask_admin.contrib import geoa
 
 class MicroBlogModelView(ModelView):
-    
+    column_exclude_list = ['password']
     def is_accessible(self):
         if current_user.is_authenticated:
             if current_user.role == 'admin':
@@ -16,6 +16,7 @@ class MicroBlogModelView(ModelView):
         # redirect to login page if user doesn't have access
         return redirect(url_for('login'))
 class GeoModelView(geoa.ModelView):
+    column_exclude_list = ['geometry']
     # form_widget_args = {'geometry': {'data-height': 400, 'data-width': 400}}
     def is_accessible(self):
         if current_user.is_authenticated:
