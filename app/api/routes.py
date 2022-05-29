eb8e3b1e8aeabb14a271209ca402beb081fc064f
Odilxon <mmuhtor@gmail.com>
2022-05-29 18:01:42 +0500

    api

---
+++ app/api/routes.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, jsonify
+from app import db, cache
+from app.main.models import District, Province
+from flask_login import current_user, login_required
+from geoalchemy2.functions import ST_AsGeoJSON
+import json
+api = Blueprint("api",__name__, url_prefix='/api')
+
+@api.route("/district", methods=['GET'])
+@login_required
+def get_district():
+    @cache.memoize(3600)
+    def generate_geojson(dist_id):
+        dist = District.query.get(dist_id)
+        prc = Province.query.get(dist.region_id)
+        rest = db.session.query(ST_AsGeoJSON(District.geometry), District.nameru).filter(District.region_id==prc.id, District.district_prefix==dist.district_prefix).first()
+        print(rest)
+        FeatureCollection = {
+            "type" : "FeatureCollection",
+            "features" : [],
+            
+        }
+        feature = {
+            "type" : "Feature"
+        }
+        geometry = rest[0]
+        geometry = json.loads(geometry)
+        feature['geometry'] = geometry
+        feature['properties'] = {
+            "nameru": rest[1] 
+        }
+        FeatureCollection['features'].append(feature)
+        return FeatureCollection    
+    
+    
+    return jsonify(generate_geojson(current_user.district_id))
+
