93e9156ca7b3fd7e2075400819c6ad72388a2255
Odilxon <mmuhtor@gmail.com>
2022-05-29 19:27:48 +0500

    debugtoolbar

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,14 @@ from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_caching import Cache
-
+from flask_debugtoolbar import DebugToolbarExtension
 
 login_manager = LoginManager()
 admin = Admin(name='microblog', template_mode='bootstrap4')
 db = SQLAlchemy()
 migrate = Migrate()
 cache = Cache()
-
+toolbar = DebugToolbarExtension()
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('configs/config.py')
@@ -21,7 +21,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     cache.init_app(app)
-    
+    toolbar.init_app(app)
     from app.main.routes import main
     from app.api.routes import api
     app.register_blueprint(main)
@@ -34,11 +34,11 @@ def create_app():
         header.add('Access-Control-Allow-Headers', '*')
         header.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
         return response
-    from app.main.admin import MicroBlogModelView
+    from app.main.admin import MicroBlogModelView, GeoModelView
     from app.main.models import User, Province, District, Permission, CropName
     admin.add_view(MicroBlogModelView(User, db.session))
-    admin.add_view(MicroBlogModelView(Province, db.session))
-    admin.add_view(MicroBlogModelView(District, db.session))
+    admin.add_view(GeoModelView(Province, db.session))
+    admin.add_view(GeoModelView(District, db.session))
     admin.add_view(MicroBlogModelView(Permission, db.session))
     admin.add_view(MicroBlogModelView(CropName, db.session))
 
