1eb59d7f9845bbeced163624c00c75cdd966720f
Odilxon <mmuhtor@gmail.com>
2022-05-29 19:28:28 +0500

    Geoalchemy view

--- app/main/admin.py
+++ app/main/admin.py
@@ -2,8 +2,21 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import redirect, url_for
+from flask_admin.contrib import geoa
+
 class MicroBlogModelView(ModelView):
     
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            if current_user.role == 'admin':
+                return True
+        return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for('login'))
+class GeoModelView(geoa.ModelView):
+    # form_widget_args = {'geometry': {'data-height': 400, 'data-width': 400}}
     def is_accessible(self):
         if current_user.is_authenticated:
             if current_user.role == 'admin':
