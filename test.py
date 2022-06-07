87729777d1fee4fb09cb65c2dbea1917d139e378
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 20:46:09 +0500

    'Commited'

--- test.py
+++ test.py
@@ -160,4 +160,4 @@ def create_dis():
                     dump = dump + ")"
                 dist = District(
                     nameru = d['properties']['name'],
-                    district_p
+                    district_pr
