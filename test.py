7fbbfe98d07f6b5679ecdb36461b224a3c1a2700
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 20:46:13 +0500

    'Commited'

--- test.py
+++ test.py
@@ -160,4 +160,4 @@ def create_dis():
                     dump = dump + ")"
                 dist = District(
                     nameru = d['properties']['name'],
-                    district_prefix = str(d['properties']['cadastr_nu
+                    district_prefix = str(d['properties']['cadastr_num
