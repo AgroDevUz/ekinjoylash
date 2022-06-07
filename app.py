ad765554b1d522878bf1ddf60c89cdb00db0bd21
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 20:52:49 +0500

    'Commited'

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import os
+import time
+from alive_progress import alive_bar
 
 total = 0
 ignore = ""
 
-with open("test.py", "r") as f:
+with open("map.md", "r") as f:
     ignore = f.read()
     total = len(ignore)
-    print(f.read())
     
-with open("map.md", "w") as f:
+
+with open("leaflet.md", "w") as f:
     f.write("")
 
 
-
-for i in range(total):
-    with open("test.py", "a") as f:
-        f.write(ignore[i])
-    os.system("git add .")  
-    os.system("git commit -m 'Commited'")
-    if i % 100 == 0:
-        os.system("git push")
-    
+with alive_bar(total) as bar:
+    for i in range(total):
+        with open("leaflet.md", "a") as f:
+            f.write(ignore[i])
+        os.system("git add .")  
+        os.system("git commit -m 'Commited'")
+        if i % 100 == 0:
+            os.system("git push")
+        bar()
 
 os.system("git push")  
