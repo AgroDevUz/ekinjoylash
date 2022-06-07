bf5cc40e88b6509ca43dcc3c4c4f505192c4fe5a
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 20:45:24 +0500

    'Commited'

--- test.py
+++ test.py
@@ -149,4 +149,4 @@ def create_dis():
     with open("tuman.json", 'r', encoding='utf8') as f:
         data = json.loads(f.read())
         for d in data['features']:
-            ds = District.query.filter_by(nameru=d['properties']['
+            ds = District.query.filter_by(nameru=d['properties']['n
