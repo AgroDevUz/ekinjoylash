03829a2055e1c3e6ea1dd8013a32c4b5f404c288
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 20:45:18 +0500

    'Commited'

--- test.py
+++ test.py
@@ -148,4 +148,4 @@ def create_user():
 def create_dis():
     with open("tuman.json", 'r', encoding='utf8') as f:
         data = json.loads(f.read())
-        for d in data['fe
+        for d in data['fea
