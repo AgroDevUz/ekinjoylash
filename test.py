82f0e33d84702cf9096e945ad24f7de3dbab36f1
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 20:44:07 +0500

    'Commited'

--- test.py
+++ test.py
@@ -122,4 +122,4 @@ def create_admin():
     u = User(
         login = 'admin',
         role = 'admin',
-        district_id = District.query.filter_b
+        district_id = District.query.filter_by
