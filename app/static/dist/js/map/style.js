36d8fd433495e24406d0b28db5102fa6a6459b5b
Malik <snapfirecookie@gmail.com>
2022-06-21 16:04:40 +0500

    Data table, filter, layer colors

--- app/static/dist/js/map/style.js
+++ app/static/dist/js/map/style.js
@@ -1,7 +1,7 @@
 var districtStyle = {
     fillColor: "transparent",
-    color: "blue",
-    weight: 2,
+    color: "white",
+    weight: 2.25,
     dashArray: '3',
     opacity: 1,
     fillOpacity: 0.8
@@ -13,8 +13,8 @@ function farmerStyle(feature) {
     return {
     color: "white",
     dashArray: '3',
-    fillColor: farmColor(feature.properties.baunit_type),
-    weight: 1,
+    fillColor: farmerColors[tables[feature.properties.baunit_type]],
+    weight: 0.75,
     opacity: 1,
     fillOpacity: 0.8
     }
@@ -28,23 +28,21 @@ function farmerStyle(feature) {
 
 //bu_type_land_reserve Zaxiradagi qishloq xo‘jaligiga mo‘ljallangan yer uchastkasi
 
-function farmColor(d){
-    if (d == 'bu_type_land_farm') {
-        return "blue"
-    }
-    if (d == 'bu_type_land_dehkan_auction') {
-        return "yellow"
-    }
-    if (d == 'bu_type_agricultural_enterprises') {
-        return "green"
-    }
-    if (d == 'bu_type_land_private') {
-        return "green"
-    }
-    if (d == 'bu_type_land_dehkan') {
-        return "red"
-    }else {
-        return "pink"
-    }
+const tables = {
+    "bu_type_land_ancillary": '1',
+    "bu_type_land_highways": '2',
+    "bu_type_land_dehkan": '3',
+    "bu_type_bad_land": '4',
+    "bu_type_land_r_and_d": '5',
+    "bu_type_land_farm": '6',
+    "bu_type_land_pasture": '7',
+    "bu_type_land_private": '8',
+    "bu_type_land_dehkan_auction": '9',
+    "bu_type_land_farm_auction": '10',
+    "bu_type_land_reserve": '11',
+    "bu_type_agricultural_enterprises": '12',
+    "bu_type_land_forest": '13',
+    "bu_type_land_household": '14',
+};
 
-}
+const farmerColors = [ "#248DCE", "#2A99E2", "#2D63E2", "#10A3C4", "#137DC4", "#5AB2C6", "#16BDE2", "#469DB0", "#196096", "#2FA19D", "#3DD5CD", "#425CA4", "#54B0E1", "#4884DF" ];
