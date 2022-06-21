fb6c3c173a74877a46d3591da5c2e1937309176e
abrornomos <abrornomos2018@gmail.com>
2022-06-21 10:54:19 +0500

    Table added

--- app/api/crop/routes.py
+++ app/api/crop/routes.py
@@ -161,7 +161,7 @@ def get_crop_geojson():
                 'features': []
             }
         }
-    crops_obj = db.session.query(Crop.crop_id, ST_AsGeoJSON(Crop.geometry), Crop.area, Crop.district_id, Crop.farm_tax_number, Crop.farm_cad_number, Crop.user_id, Crop.created_at, Crop.updated_at, Crop.productivity, Crop.contour_number, CropName.name, Crop.land_type_id, Crop.planting_type_id, Crop.planting_method_id)\
+    crops_obj = db.session.query(Crop.crop_id, ST_AsGeoJSON(Crop.geometry), Crop.area, Crop.district_id, Crop.farm_tax_number, Crop.farm_cad_number, Crop.user_id, Crop.created_at, Crop.updated_at, Crop.productivity, Crop.contour_number, CropName.name, LandType.name, PlantingType.name, PlantingMethod.name)\
         .join(CropName, CropName.id == Crop.crop_id)\
         .join(LandType, LandType.id == Crop.land_type_id)\
         .join(PlantingType, PlantingType.id == Crop.planting_type_id)\
@@ -171,7 +171,7 @@ def get_crop_geojson():
         "type": "FeatureCollection",
         "features": []
     }
-    for crop_id, crop_geo, crop_area, crop_district_id, crop_farm_tax_number, crop_farm_cad_number, user_id, created_at, updated_at, ball_bonitet, contour_number, cropname in crops_obj:
+    for crop_id, crop_geo, crop_area, crop_district_id, crop_farm_tax_number, crop_farm_cad_number, user_id, created_at, updated_at, ball_bonitet, contour_number, cropname, crop_land_type, crop_planting_type, crop_planting_method in crops_obj:
 
         feature = {
             "type": "Feature"
@@ -188,7 +188,10 @@ def get_crop_geojson():
             'created_at': created_at,
             'updated_at': updated_at,
             'ball_bonitet': ball_bonitet,
-            'contour_number': contour_number
+            'contour_number': contour_number,
+            'land_type': crop_land_type,
+            'planting_type': crop_planting_type,
+            'planting_method': crop_planting_method
         }
         code = CropName.query.get(int(crop_id)).code
         crops[code]['feature_collection']['features'].append(feature)
@@ -197,32 +200,48 @@ def get_crop_geojson():
 @crop.route('/get-select-data')
 def get_select_data():
     data = {
-        'crops': [],
-        'land_types': [],
-        'planting_types': [],
-        'planting_methods': [],
+        'crops': {
+            'name': "Ekin nomi",
+            'key': "crop_name",
+            'items': []
+        },
+        'land_types': {
+            'name': "Yer turi",
+            'key': "land_type_id",
+            'items': []
+        },
+        'planting_types': {
+            'name': "Ekish turi",
+            'key': "planting_type_id",
+            'items': []
+        },
+        'planting_methods': {
+            'name': "Ekish usuli",
+            'key': "planting_method_id",
+            'items': []
+        },
     }
     crops = CropName.query.all()
     for crop in crops:
-        data['crops'].append({
+        data['crops']['items'].append({
             'id': crop.code,
             'name': crop.name,
         })
     land_types = LandType.query.all()
     for land_type in land_types:
-        data['land_types'].append({
+        data['land_types']['items'].append({
             'id': land_type.id,
             'name': land_type.name,
         })
     planting_types = PlantingType.query.all()
     for planting_type in planting_types:
-        data['planting_types'].append({
+        data['planting_types']['items'].append({
             'id': planting_type.id,
             'name': planting_type.name,
         })
     planting_methods = PlantingMethod.query.all()
     for planting_method in planting_methods:
-        data['planting_methods'].append({
+        data['planting_methods']['items'].append({
             'id': planting_method.id,
             'name': planting_method.name,
         })
@@ -253,3 +272,102 @@ def calc_area():
         area = geom_area.area / 10000
         return jsonify({"area": area})
     return jsonify({"error": "Method not allowed"}), 405
+
+
+@crop.route("/get-crop-data-by-farmer", methods=['POST'])
+def get_crop_data_by_farmer():
+    if request.method == 'POST':
+        try:
+            req = request.get_json()
+        except Exception as e:
+            return jsonify({"error": str(e)})
+        cadastral_number = req.get('cadastral_number')
+        # crops = Crop.query.filter(Crop.farm_cad_number==cadastral_number).all()
+        crops = db.session.query(Crop.area, Crop.contour_number, CropName.code, Crop.land_type_id, Crop.planting_type_id, Crop.planting_method_id)\
+            .join(CropName, CropName.id == Crop.crop_id)\
+            .filter(Crop.farm_cad_number==req.get('cadastral_number')).all()
+        temp = {
+            "contour_number": "",
+            "jami": 0,
+            "texnik": {
+                "jami": 0,
+                "guza": 0,
+                "kannabis": 0,
+                "tamaki": 0
+            },
+            "boshokli": {
+                "jami": 0,
+                "102010000": 0,
+                "arpa": 0,
+                "tritikale": 0
+            },
+            "sholi": 0,
+            "dukkakli": {
+                "jami": 0,
+                "nuxat": 0,
+                "mosh": 0,
+                "loviya": 0,
+                "qizil_loviya": 0,
+                "yeryongoq": 0
+            },
+            "moyli": {
+                "jami": 0,
+                "soya": 0,
+                "kungaboqar": 0,
+                "kunjut": 0,
+                "sedana": 0,
+                "raps": 0
+            },
+            "sabzavot": 0,
+            "poliz": {
+                "jami": 0,
+                "tarvuz": 0,
+                "qovun": 0,
+                "qovoq": 0
+            },
+            "107010000": 0,
+            "ozuqa": {
+                "jami": 0,
+                "beda": 0,
+                "xashaki_lavlagi": 0,
+                "oq_joxori": 0,
+                "makka_don": 0,
+                "makka_silos": 0
+            },
+            "dorivor": 0,
+            "supurgi": 0,
+            "past_ball_bonitet": {
+                "jami": 0,
+                "intensiv": 0,
+                "danakli": 0,
+                "uruglik": 0,
+                "yongoq": 0,
+                "anor": 0,
+                "zaytun": 0,
+                "malina": 0,
+                "uzumzorlar": 0,
+                "tutzorlar": 0
+            }
+        }
+        data = []
+        for area, contour_number, code, land_type_id, planting_type_id, planting_method_id in crops:
+            temp_ = temp
+            temp_['contour_number'] = contour_number
+            if str(code) in temp.keys():
+                temp_[str(code)] += area
+                temp_['jami'] += area
+            else:
+                for key, value in temp_.items():
+                    if isinstance(value, dict):
+                        if str(code) in value.keys():
+                            temp_[key][str(code)] += area
+                            temp_[key]['jami'] += area
+            new_temp = []
+            for key, value in temp_.items():
+                if isinstance(value, dict):
+                    for key_, value_ in value.items():
+                        new_temp.append(value_)
+                else:
+                    new_temp.append(value)
+            data.append(new_temp)
+        return jsonify(data)
